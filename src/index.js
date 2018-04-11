0bdf2d82aafe06db36bfaa33e2854b0741457d5c
raphsutti <neoster9@gmail.com>
2018-04-11 21:08:41 +1000

    youtube api search test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 // YouTube API key - console.developers.google.com
 const API_KEY = 'AIzaSyDYomOy8E018K4w5b5UxDQoImpGecVm5H4';
+
+YTSearch({key: API_KEY, term: 'surfbaords'}, function(data) {
+  console.log(data);
+})
 
 // Create new component. This component should produce some HTML
 const App = () => {
