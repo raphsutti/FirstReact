213704fc33256c14ad8ed3223575f41707bdfb65
raphsutti <neoster9@gmail.com>
2018-04-12 06:50:58 +1000

    Change const to class for state

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
@@ -10,13 +10,14 @@ YTSearch({key: API_KEY, term: 'surfbaords'}, function(data) {
   console.log(data);
 })
 
-// Create new component. This component should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
