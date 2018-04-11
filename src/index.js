46321cb67948774d1e582bb23b3c14c53bcc6c7f
raphsutti <neoster9@gmail.com>
2018-04-12 06:56:14 +1000

    search function in class component

--- src/index.js
+++ src/index.js
@@ -6,11 +6,18 @@ import SearchBar from './components/search_bar';
 // YouTube API key - console.developers.google.com
 const API_KEY = 'AIzaSyDYomOy8E018K4w5b5UxDQoImpGecVm5H4';
 
-YTSearch({key: API_KEY, term: 'surfbaords'}, function(data) {
-  console.log(data);
-})
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfbaords'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+
   render () {
     return (
       <div>
