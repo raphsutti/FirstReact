52aaa8ffb91716dd5f71d36d51a04c876cb49550
raphsutti <neoster9@gmail.com>
2018-04-13 07:04:41 +1000

    fix video size

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -13,7 +13,7 @@ const VideoDetail = ({video}) => {
   // component
   return (
     <div className="video-detail col-md-8">
-      <div className="embed-rsponsive embed-responsive-16by8">
+      <div className="embed-responsive embed-responsive-16by9">
         <iframe className="embed-responsive-item" src={url}></iframe>
       </div>
       <div className="details">
