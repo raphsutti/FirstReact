5e3ea156a16697239d46fe9de12fdfa755c02cd1
raphsutti <neoster9@gmail.com>
2018-04-17 06:46:02 +1000

    search term functionality

--- src/index.js
+++ src/index.js
@@ -18,7 +18,13 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    this.videoSearch('surfboards');
+    
+  }
+
+  // Search term method
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({ 
         videos:videos,
         selectedVideo: videos[0]
@@ -30,7 +36,7 @@ class App extends Component {
   render () {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList 
           onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
